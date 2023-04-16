49aeeca0befa551520d598d7218c150affbc9df2
Krish Halder <geonextgis@gmail.com>
2023-04-16 21:56:13 +0530

    repository updated

--- geolens/__init__.py
+++ geolens/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """Krishnagopal Halder"""
 __email__ = 'geonextgis@gmail.com'
 __version__ = '0.0.1'
+
+from .geolens import *
--- geolens/geolens.py
+++ geolens/geolens.py
@@ -1,1 +1,16 @@
 """Main module."""
+
+import string
+import random
+
+def generate_random_password(length=10, upper=False, digits=False, punctuation=False):
+    letters = string.ascii_lowercase
+    if upper:
+        letters += string.ascii_uppercase
+    if digits:
+        letters += string.digits
+    if punctuation:
+        letters += string.punctuation
+    password = "".join(random.choice(letters) for i in range(length))
+    return password
+
