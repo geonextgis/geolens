ef2c8ac20184a1d46bb6570d8b7be9844db57e31
Krish Halder <geonextgis@gmail.com>
2023-04-17 18:41:34 +0530

    Add docstrings

--- geolens/geolens.py
+++ geolens/geolens.py
@@ -4,6 +4,17 @@ import string
 import random
 
 def generate_random_password(length=10, upper=False, digits=False, punctuation=False):
+    """Generates a random password of a given length.
+
+    Args:
+        length (int, optional): The length of the string to generate. Defaults to 10.
+        upper (bool, optional): Whether to include uppercase letters. Defaults to False.
+        digits (bool, optional): Whether to include digits. Defaults to False.
+        punctuation (bool, optional): Whether to include punctuation. Defaults to False.
+
+    Returns:
+        str: The generated password.
+    """
     letters = string.ascii_lowercase
     if upper:
         letters += string.ascii_uppercase
@@ -16,6 +27,14 @@ def generate_random_password(length=10, upper=False, digits=False, punctuation=F
 
 
 def generate_lucky_number(length=1):
+    """Generates a random number of given length.
+
+    Args:
+        length (int, optional): The length of the number to generate. Defaults to 1.
+
+    Returns:
+        int: The generated number.
+    """
     digit = string.digits
     lucky_number = "".join(random.choice(digit) for i in range(length))
     return int(lucky_number)
